43e9cc9e0ad572848382dae093049dae27ed1079
Albner0902 <1204244849@qq.com>
2017-06-01 00:27:51 +1000

    Initial Commit

---
+++ ASA Health Clinic/PatientsDetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  ASA Health Clinic
+//
+//  Created by zhenyu on 31/5/17.
+//  Copyright © 2017 zhenyu. All rights reserved.
+//
+
+import UIKit
+
+class PatientsDetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
